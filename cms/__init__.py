c7bcb35662821445b12a586159f77929bda2c30d
Dipin <dgargdipin@gmail.com>
2020-11-14 12:43:19 +0530

    for deployment

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 import os
 from flask_login import LoginManager
 app =Flask(__name__)
@@ -9,7 +8,6 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get['database_uri']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
-Migrate(app,db,render_as_batch=True)
 login_manager=LoginManager()
 login_manager.init_app(app)
 login_manager.login_view='users.login'
